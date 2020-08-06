3009aec760b7efe6c1259e7883b2233ca67b9528
jpernesz <1020894@student.sae.edu.au>
2020-08-06 20:53:46 +1000

    Building Monster Left Arm

---
+++ Assets/Scripts/Audio.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Build.Player;
+using UnityEngine;
+
+public class Audio : MonoBehaviour
+{
+    public AudioSource ourAudioSource;
+    //public PlayTheAudio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //if(ourAud
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
